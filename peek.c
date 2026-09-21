795361b1e6bb5a6f4bfb70e68f3357e0b9c05c67
Pranitha-2270 <saipranitha802@gmail.com>
2026-09-21 11:58:53 +0530

    Add files via upload
    peek

---
+++ peek.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX 100
+int stack[MAX];
+int top = -1;
+void peek() {
+if (top == -1) {
+printf("Stack is empty\n");
+} else {
+printf("Top element: %d\n", stack[top]);
+}
+}
+int main() {
+stack[++top] = 10;
+stack[++top] = 20;
+stack[++top] = 30;
+peek();
+return 0;
+}
+
